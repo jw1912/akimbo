67c321434304fdce0c7f2557256154955ca52968
Jamie Whiting <99771266+JacquesRW@users.noreply.github.com>
2023-08-04 12:03:34 +0100

    Generation 6 of Datagen/Tuning (#82)
    Bench: 5627890

--- datagen/src/thread.rs
+++ datagen/src/thread.rs
@@ -16,6 +16,7 @@ pub struct ThreadData {
     games: u64,
     fens: u64,
     start_time: Instant,
+    pub seed_positions: Vec<String>,
 }
 
 impl ThreadData {
@@ -43,6 +44,7 @@ impl ThreadData {
             games: 0,
             fens: 0,
             start_time: Instant::now(),
+            seed_positions: Vec::new(),
         };
 
         res.engine.resize_tt(hash_size);
@@ -97,25 +99,32 @@ impl ThreadData {
     pub fn run_game(&mut self) -> Option<GameResult>{
         self.reset();
 
-        let mut position = Position::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        let mut position;
 
-        // play 8 or 9 random moves
-        for _ in 0..(8 + (self.rng() % 2)) {
-            let moves = position.movegen::<true>();
+        if let Some(fen) = self.seed_positions.get(self.games as usize) {
+            position = Position::from_fen(fen);
+        } else {
+            // use startpos
+            position = Position::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
-            let mut legals = Vec::new();
-            for &mov in &moves.list[..moves.len] {
-                let mut new = position;
-                if !new.make(mov) {
-                    legals.push(mov);
+            // play 8 or 9 random moves
+            for _ in 0..(8 + (self.rng() % 2)) {
+                let moves = position.movegen::<true>();
+
+                let mut legals = Vec::new();
+                for &mov in &moves.list[..moves.len] {
+                    let mut new = position;
+                    if !new.make(mov) {
+                        legals.push(mov);
+                    }
                 }
-            }
 
-            if legals.is_empty() {
-                return None;
-            }
+                if legals.is_empty() {
+                    return None;
+                }
 
-            position.make(legals[self.rng() as usize % legals.len()]);
+                position.make(legals[self.rng() as usize % legals.len()]);
+            }
         }
 
         let mut result = GameResult::default();
