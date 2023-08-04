afc2fb85beddfe765d856adcfa8b432cf42fcbf7
Jamie Whiting <99771266+JacquesRW@users.noreply.github.com>
2023-08-04 23:05:15 +0100

    Generation 7 of Datagen/Tuning (#83)
    ELO   | -46.73 +- 17.18 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | -3.00 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 1152 W: 336 L: 490 D: 326
    https://chess.swehosting.se/test/3091/
    
    Bench: 5880568

--- tuner/src/main.rs
+++ tuner/src/main.rs
@@ -6,7 +6,7 @@ use tuner::{Data, gd_tune};
 use crate::core::{Params, S};
 
 fn main() -> std::io::Result<()> {
-    let file_name = args().nth(1).unwrap_or(String::from("resources/akimbo-gen5-40k.epd"));
+    let file_name = args().nth(1).unwrap_or(String::from("resources/combined.epd"));
     // initialise data
     let mut data = Data::default();
     data.1 = 4;
