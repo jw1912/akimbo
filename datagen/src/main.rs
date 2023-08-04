3f92988d283bc639c6f34a0361fd6fe4435cee96
JacquesRW <jamie.whiting2003@icloud.com>
2023-08-04 19:53:37 +0100

    ye

--- datagen/src/main.rs
+++ datagen/src/main.rs
@@ -6,34 +6,23 @@ use std::{
     fs::File,
     io::{BufRead, BufReader},
     sync::atomic::{AtomicBool, Ordering},
-    thread::{spawn, sleep}
+    thread::spawn,
 };
 
 pub static STOP: AtomicBool = AtomicBool::new(false);
 
-const GAMES_PER_THREAD: usize = 20_000;
-const NODES_PER_GAME: u64 = 40_000;
+const GAMES_PER_THREAD: usize = 10_000;
+const NODES_PER_GAME: u64 = 10_000;
 
 fn main() {
     let mut handles = Vec::new();
 
-    let noob8 = File::open("resources/books/8moves_v3.epd").unwrap();
+    let book = File::open("resources/books/8moves_v3.epd").unwrap();
 
-    let noob8_vec = BufReader::new(noob8).lines().map(|ln| ln.unwrap()).take(GAMES_PER_THREAD).collect::<Vec<String>>();
+    let book_vec = BufReader::new(book).lines().map(|ln| ln.unwrap()).collect::<Vec<String>>();
 
-    sleep(std::time::Duration::from_millis(500));
-    handles.push(
-        spawn(move || {
-            let mut worker = ThreadData::new(NODES_PER_GAME, 8);
-            worker.seed_positions = noob8_vec;
-            worker.run_datagen(GAMES_PER_THREAD as u64);
-        })
-    );
 
-    let pohl = File::open("resources/books/Pohl.epd").unwrap();
-    let pohl_vec = BufReader::new(pohl).lines().map(|ln| ln.unwrap()).take(2 * GAMES_PER_THREAD).collect::<Vec<String>>();
-
-    for seeds in pohl_vec.chunks(GAMES_PER_THREAD / 2).take(3) {
+    for seeds in book_vec.chunks(GAMES_PER_THREAD / 4).take(4) {
         let x = seeds.to_vec();
         handles.push(
             spawn(move || {
