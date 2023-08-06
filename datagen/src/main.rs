0ff918868ad85282e42bfa07402ac5d6d9d901f5
Jamie Whiting <99771266+JacquesRW@users.noreply.github.com>
2023-08-06 17:35:56 +0100

    Cleanup Datagen and Tuning Code (#88)
    Bench: 5563040

--- datagen/src/main.rs
+++ datagen/src/main.rs
@@ -3,8 +3,6 @@ mod thread;
 
 use thread::ThreadData;
 use std::{
-    fs::File,
-    io::{BufRead, BufReader},
     sync::atomic::{AtomicBool, Ordering},
     thread::spawn,
 };
@@ -12,22 +10,15 @@ use std::{
 pub static STOP: AtomicBool = AtomicBool::new(false);
 
 const GAMES_PER_THREAD: usize = 10_000;
-const NODES_PER_GAME: u64 = 10_000;
+const NODES_PER_GAME: u64 = 40_000;
 
 fn main() {
     let mut handles = Vec::new();
 
-    let book = File::open("resources/books/8moves_v3.epd").unwrap();
-
-    let book_vec = BufReader::new(book).lines().map(|ln| ln.unwrap()).collect::<Vec<String>>();
-
-
-    for seeds in book_vec.chunks(GAMES_PER_THREAD / 4).take(4) {
-        let x = seeds.to_vec();
+    for _ in 0..4 {
         handles.push(
             spawn(move || {
                 let mut worker = ThreadData::new(NODES_PER_GAME, 8);
-                worker.seed_positions = x;
                 worker.run_datagen(GAMES_PER_THREAD as u64);
             })
         );
