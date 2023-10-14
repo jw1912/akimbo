3941ed1360e809f4d24898e8aef0854fe9cedaa6
Jamie Whiting <99771266+jw1912@users.noreply.github.com>
2023-10-14 19:47:42 +0100

    Output Buckets (#155)
    ELO   | 19.76 +- 9.81 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.93 (-2.25, 2.89) [0.00, 5.00]
    GAMES | N: 2816 W: 901 L: 741 D: 1174
    https://chess.swehosting.se/test/4310/
    
    Bench: 3539435

--- akimbo/src/network.rs
+++ akimbo/src/network.rs
@@ -3,6 +3,7 @@ const SCALE: i32 = 400;
 const QA: i32 = 255;
 const QB: i32 = 64;
 const QAB: i32 = QA * QB;
+const OUTPUT_BUCKETS: usize = 8;
 
 #[inline]
 fn activate(x: i16) -> i32 {
@@ -13,22 +14,22 @@ fn activate(x: i16) -> i32 {
 pub struct Network {
     feature_weights: [Accumulator; 768],
     feature_bias: Accumulator,
-    boys_weights: Accumulator,
-    opps_weights: Accumulator,
-    output_bias: i16,
+    output_weights: [[Accumulator; 2]; OUTPUT_BUCKETS],
+    output_bias: [i16; OUTPUT_BUCKETS],
 }
 
 static NNUE: Network = unsafe { std::mem::transmute(*include_bytes!("../../resources/net.bin")) };
 
 impl Network {
-    pub fn out(boys: &Accumulator, opps: &Accumulator) -> i32 {
-        let mut sum = i32::from(NNUE.output_bias);
+    pub fn out(boys: &Accumulator, opps: &Accumulator, occ: u64) -> i32 {
+        let bucket = (occ.count_ones() - 2) as usize / 4;
+        let mut sum = i32::from(NNUE.output_bias[bucket]);
 
-        for (&x, &w) in boys.vals.iter().zip(&NNUE.boys_weights.vals) {
+        for (&x, &w) in boys.vals.iter().zip(&NNUE.output_weights[bucket][0].vals) {
             sum += activate(x) * i32::from(w);
         }
 
-        for (&x, &w) in opps.vals.iter().zip(&NNUE.opps_weights.vals) {
+        for (&x, &w) in opps.vals.iter().zip(&NNUE.output_weights[bucket][1].vals) {
             sum += activate(x) * i32::from(w);
         }
 
