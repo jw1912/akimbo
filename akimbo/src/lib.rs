3666b4f869a81f8f150797b129ac7370ddc5e6f5
JacquesRW <jamie.whiting2003@icloud.com>
2023-10-18 18:49:36 +0100

    Boxes u Bench: 3316179

--- akimbo/src/lib.rs
+++ akimbo/src/lib.rs
@@ -41,3 +41,14 @@ macro_rules! init {
         res
     }}
 }
+
+pub fn boxed_and_zeroed<T>() -> Box<T> {
+    unsafe {
+        let layout = std::alloc::Layout::new::<T>();
+        let ptr = std::alloc::alloc_zeroed(layout);
+        if ptr.is_null() {
+            std::alloc::handle_alloc_error(layout);
+        }
+        Box::from_raw(ptr.cast())
+    }
+}
