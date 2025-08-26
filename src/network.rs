896c2c3b4c974c1f9a719edb39478b23237f48c5
jw1912 <jamie.whiting.dev@gmail.com>
2025-08-26 18:07:10 +0100

    Bench: 2256851

--- src/network.rs
+++ src/network.rs
@@ -31,10 +31,23 @@ static BUCKETS: [usize; 64] = [
     3, 3, 3, 3, 7, 7, 7, 7,
 ];
 
+fn activ_dot_prod(acc: &Accumulator, weights: &Accumulator) -> i32 {
+    let mut sum = 0;
+
+    for (&x, &w) in acc.vals.iter().zip(&weights.vals) {
+        // Lizard trick
+        let v = x.clamp(0, QA as i16);
+        let vw = v * w;
+        sum += i32::from(v) * i32::from(vw);
+    }
+
+    sum
+}
+
 impl Network {
     pub fn out(boys: &Accumulator, opps: &Accumulator) -> i32 {
         let weights = &NNUE.output_weights;
-        let sum = flatten(boys, &weights[0]) + flatten(opps, &weights[1]);
+        let sum = activ_dot_prod(boys, &weights[0]) + activ_dot_prod(opps, &weights[1]);
         (sum / QA + i32::from(NNUE.output_bias)) * SCALE / QAB
     }
 
@@ -130,81 +143,5 @@ impl Default for EvalTable {
         }
 
         Self { table }
-    }
-}
-
-fn flatten(acc: &Accumulator, weights: &Accumulator) -> i32 {
-    #[cfg(not(target_feature = "avx2"))]
-    {
-        fallback::flatten(acc, weights)
-    }
-    #[cfg(target_feature = "avx2")]
-    unsafe {
-        avx2::flatten(acc, weights)
-    }
-}
-
-#[cfg(not(target_feature = "avx2"))]
-mod fallback {
-    use super::{Accumulator, QA};
-
-    #[inline]
-    pub fn screlu(x: i16) -> i32 {
-        i32::from(x.clamp(0, QA as i16)).pow(2)
-    }
-
-    #[inline]
-    pub fn flatten(acc: &Accumulator, weights: &Accumulator) -> i32 {
-        let mut sum = 0;
-
-        for (&x, &w) in acc.vals.iter().zip(&weights.vals) {
-            sum += screlu(x) * i32::from(w);
-        }
-
-        sum
-    }
-}
-
-#[cfg(target_feature = "avx2")]
-mod avx2 {
-    use super::{Accumulator, HIDDEN, QA};
-    use std::arch::x86_64::*;
-
-    pub unsafe fn flatten(acc: &Accumulator, weights: &Accumulator) -> i32 {
-        use std::arch::x86_64::*;
-
-        const CHUNK: usize = 16;
-
-        let mut sum = _mm256_setzero_si256();
-        let min = _mm256_setzero_si256();
-        let max = _mm256_set1_epi16(QA as i16);
-
-        for i in 0..HIDDEN / CHUNK {
-            let mut v = load_i16s(acc, i * CHUNK);
-            v = _mm256_min_epi16(_mm256_max_epi16(v, min), max);
-            let w = load_i16s(weights, i * CHUNK);
-            let product = _mm256_madd_epi16(v, _mm256_mullo_epi16(v, w));
-            sum = _mm256_add_epi32(sum, product);
-        }
-
-        horizontal_sum_i32(sum)
-    }
-
-    #[inline]
-    unsafe fn load_i16s(acc: &Accumulator, start_idx: usize) -> __m256i {
-        _mm256_load_si256(acc.vals.as_ptr().add(start_idx).cast())
-    }
-
-    #[inline]
-    unsafe fn horizontal_sum_i32(sum: __m256i) -> i32 {
-        let upper_128 = _mm256_extracti128_si256::<1>(sum);
-        let lower_128 = _mm256_castsi256_si128(sum);
-        let sum_128 = _mm_add_epi32(upper_128, lower_128);
-        let upper_64 = _mm_unpackhi_epi64(sum_128, sum_128);
-        let sum_64 = _mm_add_epi32(upper_64, sum_128);
-        let upper_32 = _mm_shuffle_epi32::<0b00_00_00_01>(sum_64);
-        let sum_32 = _mm_add_epi32(upper_32, sum_64);
-
-        _mm_cvtsi128_si32(sum_32)
     }
 }
